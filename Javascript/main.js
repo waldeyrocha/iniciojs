609ad8d8c1d4693445c8009c1f58acd1a05cdaa8
Waldey <123546326+Rochapw@users.noreply.github.com>
2023-05-11 17:02:23 +0200

    Add files via upload

---
+++ Javascript/main.js
@@ -0,0 +1,74 @@
+
+alert('Hello World!')
+
+
+const numberOne = 20;
+const numberTwo = 1;
+
+const sum = numberOne + numberTwo
+
+alert(sum)
+
+
+const isNumber = 11;
+
+if (typeof isNumber == 'number') {
+  alert('É um número')
+} else {
+  alert('Não é um número')
+}
+
+const isString = 'Isso é uma string'
+
+if (typeof isString == 'string') {
+  alert('É uma string')
+} else {
+  alert('Não é uma string')
+}
+
+
+const isBoolean = true
+
+if (typeof isBoolean == 'boolean') {
+  alert('É um booleano')
+} else {
+  alert('Não é um booleano')
+}
+
+
+
+const sub = numberOne - numberTwo
+
+alert(sub)
+
+
+
+const mult = numberOne * numberTwo
+
+alert(mult)
+
+
+
+const div = numberOne / numberTwo
+
+alert(div)
+
+
+
+const isEvenNumber = 4
+
+if (isEvenNumber % 2 == 0) {
+  alert('É um número par')
+} else {
+  alert('Não é um número par')
+}
+
+
+
+const isOddNumber = 5
+
+if (isOddNumber % 2 != 0) {
+  alert('É um número ímpar')
+} else {
+  alert('Não é um número ímpar')
+}
